94109165727ffb0bed31550798e7ba406a2189ca
David Toth <dtoth2016@gmail.com>
2019-04-30 15:25:34 -0500

    Have cards displaying, need to implement game

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Wrapper from "./components/Wrapper";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import GameCard from "./components/GameCard";
-import WinModal from "./components/WinModal"
+import WinModal from "./components/WinModal";
+import items from "./items.json";
 
 class App extends Component {
 
   state = {
     score: 0,
-    topScore: 0
+    topScore: 0,
+    items
   }
 
   increment = () => {
@@ -18,6 +20,20 @@ class App extends Component {
   }
 
   checkWin = () => {
+    if (this.state.score === this.state.friends.length) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  shuffle = () => {
+    let items = this.state.items;
+      for (let i = items.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [items[i], items[j]] = [items[j], items[i]];
+      }
+    this.setState({ items: items});
   }
 
   render() {
@@ -29,6 +45,7 @@ class App extends Component {
         />
         <Hero />
         <GameCard
+          items={this.state.items}
           increment={this.increment}
           checkWin={this.checkWin}
         />
--- src/components/GameCard/index.js
+++ src/components/GameCard/index.js
@@ -1,7 +1,23 @@
 import React from "react";
+import "./style.css";
 
-function GameCard() {
-    return <div></div>
+function GameCard(props) {
+
+    const deck = props.items.map(gif => 
+        <div className="col-md-4 col-sm-6 col-12 card click-item" key={gif.id} id={gif.id} style={ {
+            backgroundImage: `url(${gif.image})`,
+            backgroundSize: "cover"
+        } }>
+        </div>
+    )
+
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                {deck}
+            </div>
+        </div>
+    )
 }
 
 export default GameCard;
