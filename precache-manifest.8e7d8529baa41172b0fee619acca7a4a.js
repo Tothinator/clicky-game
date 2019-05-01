2c72941e1f4a207faa74057f7982f4583eb1b0ad
Tothinator <dtoth2016@gmail.com>
2019-04-30 20:14:48 -0500

    Updates

---
+++ precache-manifest.8e7d8529baa41172b0fee619acca7a4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e04ec78bcc1684103eeab9f81656174b",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "15b4ad02c63f4c196c7d",
+    "url": "/clicky-game/static/css/main.decf1edd.chunk.css"
+  },
+  {
+    "revision": "61433e735fbb2d010e13",
+    "url": "/clicky-game/static/js/2.b7f52e28.chunk.js"
+  },
+  {
+    "revision": "15b4ad02c63f4c196c7d",
+    "url": "/clicky-game/static/js/main.0b2aef61.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "41a9b879d59e56d136e86a5e9ec681a7",
+    "url": "/clicky-game/static/media/synthwave_loop.41a9b879.mp4"
+  }
+]);
